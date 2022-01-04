3cf759cdaaffc1c252f8e85e9268384051606047
bigoll <rodrigobego@uol.com.br>
2022-01-04 15:18:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.alcoolougasolina
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun btCalcular(view: View){
+
+        val precoAlcool = edit_preco_alcool.text.toString()
+        val precoGasolina = edit_preco_gasolina.text.toString()
+
+        val validaCampos = validarCampos( precoAlcool, precoGasolina )
+        if(validaCampos){
+            calcularMelhorPreco( precoAlcool, precoGasolina)
+        }else{
+            text_resultado.setText("Preencha os preços primeiro!")
+        }
+
+        calcularMelhorPreco(precoAlcool, precoGasolina)
+    }
+
+    fun validarCampos(precoAlcool: String, precoGasolina: String) : Boolean{
+        var camposValidados: Boolean = true
+
+        if(precoAlcool == null||precoAlcool.equals("")){
+            camposValidados = false
+        }else if(precoGasolina == null||precoGasolina.equals("")){
+            camposValidados = false
+        }
+
+        return camposValidados
+    }
+
+
+
+    private fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String){
+
+        val valorAlcool = precoAlcool.toDouble()
+        val valorGasolina = precoGasolina.toDouble()
+
+        val resultadoPreco = valorAlcool/valorGasolina
+
+        if(resultadoPreco >= 0.7){
+            text_resultado.setText("Melhor utilizar Gasolina!")
+        }else{
+            text_resultado.setText("Melhor utilizar Álcool!")
+        }
+    }
+}
